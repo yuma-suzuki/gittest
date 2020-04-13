139c0bf580b513a97c9f59e49a4463f360c30578
yuma-suzuki <b7103.ai@gmail.com>
2020-04-13 18:35:04 +0900

    Initial Commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by 米桝 誉洋 on 2020/04/13.
+//  Copyright © 2020 Takahiro.Yonemasu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
